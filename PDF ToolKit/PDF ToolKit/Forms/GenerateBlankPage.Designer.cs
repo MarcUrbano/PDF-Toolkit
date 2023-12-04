831f8f8efefd6c7f09e79c41f264a768e206b623
MarcUrbano <marcdavidsonurbano@gmail.com>
2023-12-04 18:30:57 +0800

    [PDF]Add blank pages (#3) (#4)

---
+++ PDF ToolKit/PDF ToolKit/Forms/GenerateBlankPage.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace PDF_ToolKit
+{
+    partial class GenerateBlankPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNoOfPages = new System.Windows.Forms.TextBox();
+            this.txtOutputPath = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 32);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "No. of Pages :";
+            // 
+            // txtNoOfPages
+            // 
+            this.txtNoOfPages.Location = new System.Drawing.Point(151, 28);
+            this.txtNoOfPages.Margin = new System.Windows.Forms.Padding(4);
+            this.txtNoOfPages.Name = "txtNoOfPages";
+            this.txtNoOfPages.Size = new System.Drawing.Size(132, 22);
+            this.txtNoOfPages.TabIndex = 1;
+            // 
+            // txtOutputPath
+            // 
+            this.txtOutputPath.Location = new System.Drawing.Point(151, 60);
+            this.txtOutputPath.Margin = new System.Windows.Forms.Padding(4);
+            this.txtOutputPath.Name = "txtOutputPath";
+            this.txtOutputPath.Size = new System.Drawing.Size(132, 22);
+            this.txtOutputPath.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 64);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "PDF Output Path :";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(312, 26);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 59);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // GenerateBlankPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 114);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtOutputPath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNoOfPages);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "GenerateBlankPage";
+            this.Text = "GenerateBlankPage";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNoOfPages;
+        private System.Windows.Forms.TextBox txtOutputPath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
